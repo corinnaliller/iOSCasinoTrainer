a9104589ff475a0015da2dda1a6bcf014f2ce032
Corinna Liller / BBM2H17M <bbm2h17mli@pc09-04.bg.bib.de>
2019-04-29 12:56:12 +0200

    initial Code for SQLite

---
+++ CasinoTrainer/Additional Files/Database/DatabaseHelpers.swift
@@ -0,0 +1,35 @@
+//
+//  DatabaseHelpers.swift
+//  CasinoTrainer
+//
+//  Created by Corinna Liller / BBM2H17M on 29.04.19.
+//  Copyright © 2019 Corinna Liller. All rights reserved.
+//
+
+import Foundation
+
+let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+private enum Database: String {
+    case CasinoTrainer
+    
+    var path: String {
+        return documentsDirectory.appendingPathComponent("\(self.rawValue).sqlite").relativePath
+    }
+}
+let casinoDatabasePath = Database.CasinoTrainer.path
+
+private func destroyDatabase(db: Database) {
+    do {
+        if FileManager.default.fileExists(atPath: db.path) {
+            try FileManager.default.removeItem(atPath: db.path)
+        }
+    } catch {
+        print("Could not destroy \(db) Database file.")
+    }
+}
+enum SQLiteError: Error {
+    case OpenDatabase(message: String)
+    case Prepare(message: String)
+    case Step(message: String)
+    case Bind(message: String)
+}
---
+++ CasinoTrainer/Additional Files/Database/DatabaseTables.swift
@@ -0,0 +1,13 @@
+//
+//  DatabaseTables.swift
+//  CasinoTrainer
+//
+//  Created by Corinna Liller / BBM2H17M on 29.04.19.
+//  Copyright © 2019 Corinna Liller. All rights reserved.
+//
+
+import Foundation
+struct Players {
+    let id: Int32
+    let name: NSString
+}
